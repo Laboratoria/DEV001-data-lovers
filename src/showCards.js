319c453c1157e7cafabde40d44800609d26b85d7
gabyzam <ruth.gabriela.pz@gmail.com>
2022-10-12 13:35:14 -0500

    Agregué algunos cambios

--- src/showCards.js
+++ src/showCards.js
@@ -1,19 +1,29 @@
-import pokemon from './data/pokemon/pokemon.js'
+import pokemon from "./data/pokemon/pokemon.js";
 
-let pokeData = pokemon.pokemon
+let pokeData = pokemon.pokemon;
 //console.log (pokeData)
-let container = document.getElementById("numbers-pokemons")
+let container = document.getElementById("numbers-pokemons");
 //container.insertAdjacentHTML("afterbegin" , "<p>Holi moli</p>");
 
 //forEach recorre el array y realiza la funcion sobre cada elemento que lo compone
-    let showPokemons= pokeData.forEach ((pokemon) => {
-        let data = `<section class="cuadroPokemon"><div class="parteSuperior" id="parteSuperior"> <p class="infoUp" id="num"> ${pokemon.num} </p> <p class= "infoUp" id="region">${pokemon.generation.name}</p> <div class="photoStyle" id="photoStyle"><img src=${pokemon.img} alt="pokemones"></div>
-        <h1 class="introCard" id="introCard"> ${pokemon.name} </h1> </div>
-        <section class=parteInferior id="parteInferior"><button class "type" id="type" disabled> ${pokemon.type}</button></section>`
-       
-        container.insertAdjacentHTML("beforebegin", data);
+let showPokemons = pokeData.forEach((pokemon) => {
+  let data = `<section class="cuadroPokemon">
+  <div class="parteSuperior" id="parteSuperior"> 
+  <span class="infoUp" id="num"> ${pokemon.num} </span> 
+  <span class= "infoUp" id="region">${pokemon.generation.name}</span> 
+  
+  <div class="photoStyle" id="photoStyle">
+  <img src=${pokemon.img} alt="pokemones">
+  </div>
 
-    })
+    <h1 class="introCard" id="introCard"> ${pokemon.name} </h1> 
+    </div>
+    <section class=parteInferior id="parteInferior">
+    <div class="type" id="type" disabled> ${pokemon.type.map((type) => `<span class="type-span" >${type}</span>`).join('')}</div>
+    </section>`;
+
+  container.insertAdjacentHTML("beforebegin", data);
+});
 
 //let type= pokemonCards.filter(pokemon=> pokemon.type == "fire")
 //console.log (type)
