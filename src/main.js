25c75ecd0be5c95beea1311bcf48c21acb1a8235
pibmunoz <p6.munoz@gmail.com>
2022-10-13 15:10:16 -0300

    Co-authored-by: gabrielavillarrealdiaz <gabrielavillarrealdiaz@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -9,3 +9,4 @@ let personajesRM = data.forEach ((results) => {
     let info = `<div class="card"><img src="${results.image}"><br><p>Name: ${results.name}</p><br><p>Status: ${results.status}</p><br><p>Species: ${results.species}</p><br><p>Gender: ${results.gender}</p><br><p>Origin: ${results["origin"].name}</p></div>`;
     containerData.insertAdjacentHTML("afterbegin", info);
 })
+
