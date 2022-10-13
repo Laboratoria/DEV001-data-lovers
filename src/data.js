c1d6b0c28f8e068291ec322a1d89618801bf44fb
nicole11robayo <113997899+nicole11robayo@users.noreply.github.com>
2022-10-13 17:23:13 -0500

    llamar data en main.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,29 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
+
+  function mostrar(){
+    document.getElementById("data").value= data.books
+  }
+  /*
+  fetch("./data/harrypotter/harry.json")
+  .then(Response => Response.json())
+  .then(data => console.log(data))
+
+  
+  const mostrarData= (data.books)=>{
+    console.log(data);
+    let body="";
+    for(let i=0,)
+  }
+  */
+
   return 'example';
 };
+
+
 
 export const anotherExample = () => {
   return 'OMG';
 };
+
