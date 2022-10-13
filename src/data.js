c7df09735a00e8b632f2f564b17299f6d7773e3a
camila <khammylv@gmail.com>
2022-10-13 13:55:46 -0500

    se cambio todo segun la HU 1

--- src/data.js
+++ src/data.js
@@ -2,38 +2,41 @@ import ghibli from "./data/ghibli/ghibli.js";
 const objetos = ghibli
 
 
-const dataGhibli ={
-     datosTodasPeliculas :function(){
-      const films = objetos.films
-      let arrayFilms = [...films]
-       
-      
-      return arrayFilms
-     },
-     
-     mostrarPeliculaIndividual : function(id){
-        const arrayFilms = dataGhibli.datosTodasPeliculas()
-        const idCard = id;
-        const filmsId = arrayFilms.filter(arrayFilms => arrayFilms.id == idCard);
+const dataGhibli = {
+  datosTodasPeliculas: function () {
+    const films = objetos.films
+    let arrayFilms = [...films]
 
-        return filmsId
-     },
 
-    mostrarDataEnPantalla : function(data){
-      const datosPeliculas = data;
-       
+    return arrayFilms
+  },
 
-      return datosPeliculas;
-    },
+  mostrarPeliculaIndividual: function (id) {
+    const arrayFilms = dataGhibli.datosTodasPeliculas()
+    const idCard = id;
+    const filmsId = arrayFilms.filter(arrayFilms => arrayFilms.id == idCard);
 
-    filtrarDirectores : function(directores){
-      const arrayFilms = dataGhibli.datosTodasPeliculas();
-      const filDirectores = directores;
-      const filtroDirectores =  arrayFilms.filter(arrayFilms => arrayFilms.director == filDirectores);
-      return filtroDirectores;
-    }
+    return filmsId
+  },
+
+  mostrarDataEnPantalla: function (data) {
+    const datosPeliculas = data;
+
+
+    return datosPeliculas;
+  },
+
+  filtrarDirectores: function (directores) {
+    const arrayFilms = dataGhibli.datosTodasPeliculas();
+    const filDirectores = directores;
+    const filtroDirectores = arrayFilms.filter(arrayFilms => arrayFilms.director == filDirectores);
+    return filtroDirectores;
+  },
+
+  ordenarYears: function () {
+    //const arrayFilms = dataGhibli.datosTodasPeliculas();
     
-
+  }
 
 
 }
--- src/main.js
+++ src/main.js
@@ -16,113 +16,115 @@ function mostrarEnPantalla() {
                   <img src="${array[i].poster}" alt="${array[i].title}">
                   <p>${array[i].title}</p>
                   <p>${array[i].director}</p>
-                  
-                  <button class="select" id = "${array[i].id}" >Ver mas...</button>                    
+                  <p>${array[i].rt_score}</p>
                   </div> `;
     
     
   }
 }//<p >${array[i].description}</p>
 
+//boton para la siguiente HU 
+//<button class="select" id = "${array[i].id}" >Ver mas...</button>
+
 //este es el modal con los detalles de las peliculas al dar click
 
-const modal = document.getElementById("modal");
-const abrir = document.querySelectorAll(".select");
-const cerrar = document.getElementById("close");
-const buttons = document.querySelectorAll("button");
+// const modal = document.getElementById("modal");
+// const abrir = document.querySelectorAll(".select");
+// const cerrar = document.getElementById("close");
+// const buttons = document.querySelectorAll("button");
 
-abrir.forEach(abierto =>{
-     abierto.addEventListener("click", () => {
-      modal.style.display = "block";
-    });
-})
-  cerrar.addEventListener("click", function () {
-    modal.style.display = "none";
-});
+// abrir.forEach(abierto =>{
+//      abierto.addEventListener("click", () => {
+//       modal.style.display = "block";
+//     });
+// })
+//   cerrar.addEventListener("click", function () {
+//     modal.style.display = "none";
+// });
 
-mostrarIndividual();
-function mostrarIndividual() {
-  for (let i = 0; i < buttons.length; i++) {
-    const boton = buttons[i];
-    let idCard;
-    const pagina = document.getElementById("muestra");
-    const personajes = document.getElementById("personajes");
-    const location = document.getElementById("locacion");
-    const vehiculos = document.getElementById("vehiculos");
-    boton.addEventListener("click", () => {
-      idCard = boton.id;
-      const id = dataGhibli.mostrarPeliculaIndividual(idCard);
+// mostrarIndividual();
+// function mostrarIndividual() {
+//   for (let i = 0; i < buttons.length; i++) {
+//     const boton = buttons[i];
+//     let idCard;
+//     const pagina = document.getElementById("muestra");
+//     const personajes = document.getElementById("personajes");
+//     const location = document.getElementById("locacion");
+//     const vehiculos = document.getElementById("vehiculos");
+//     boton.addEventListener("click", () => {
+//       idCard = boton.id;
+//       const id = dataGhibli.mostrarPeliculaIndividual(idCard);
       
-      id.forEach(element => {
-        pagina.innerHTML = `
-        <img src="${element.poster}" alt="">
-        <p>${element.title}</p>
-        <p>${element.director}</p>
-        <p>${element.description}</p>
-        <p>${element.producer}</p>
-        <p>${element.release_date}</p>
-        <p>${element.rt_score}</p>
+//       id.forEach(element => {
+//         pagina.innerHTML = `
+//         <img src="${element.poster}" alt="">
+//         <p>${element.title}</p>
+//         <p>${element.director}</p>
+//         <p>${element.description}</p>
+//         <p>${element.producer}</p>
+//         <p>${element.release_date}</p>
+//         <p>${element.rt_score}</p>
          
-        `;
-          personajes.innerHTML = "";
-          element.people.forEach(persona =>{
-          personajes.innerHTML +=
-           `<div class = "cont-people">
-             <p>${persona.name}</p>
-             <p>${persona.gender}</p>
-             <p>${persona.age}</p>
-             <p>${persona.eye_color}</p>
-            <p>${persona.hair_color}</p>
-            <p>${persona.specie}</p>
-           </div>
-           ` 
-          });
+//         `;
+//           personajes.innerHTML = "";
+//           element.people.forEach(persona =>{
+//           personajes.innerHTML +=
+//            `<div class = "cont-people">
+//              <p>${persona.name}</p>
+//              <p>${persona.gender}</p>
+//              <p>${persona.age}</p>
+//              <p>${persona.eye_color}</p>
+//             <p>${persona.hair_color}</p>
+//             <p>${persona.specie}</p>
+//            </div>
+//            ` 
+//           });
           
-          location.innerHTML = ""
-          element.locations.forEach(locacion =>{
+//           location.innerHTML = ""
+//           element.locations.forEach(locacion =>{
           
-            location.innerHTML +=
-            `<div class = "cont-locacion">
-            <p>${locacion.name}</p>
-            <p>${locacion.climate}</p>
-            <p>${locacion.terrain}</p>
-            <p>${locacion.surface_water}</p>
+//             location.innerHTML +=
+//             `<div class = "cont-locacion">
+//             <p>${locacion.name}</p>
+//             <p>${locacion.climate}</p>
+//             <p>${locacion.terrain}</p>
+//             <p>${locacion.surface_water}</p>
             
-            </div>`
+//             </div>`
 
-          });
+//           });
           
-          vehiculos.innerHTML = ""
-          element.vehicles.forEach(vehiculo =>{
-            vehiculos.innerHTML +=
-            `<div class = "cont-vehiculos">
-            <p>${vehiculo.name}</p>
-            <p>${vehiculo.description}</p>
-            <p>${vehiculo.vehicle_class}</p>
-            <p>${vehiculo.length}</p>
-            <p>${vehiculo.pilot.name}</p>
+//           vehiculos.innerHTML = ""
+//           element.vehicles.forEach(vehiculo =>{
+//             vehiculos.innerHTML +=
+//             `<div class = "cont-vehiculos">
+//             <p>${vehiculo.name}</p>
+//             <p>${vehiculo.description}</p>
+//             <p>${vehiculo.vehicle_class}</p>
+//             <p>${vehiculo.length}</p>
+//             <p>${vehiculo.pilot.name}</p>
                      
-            </div>`
-          });
-        });
-       });
+//             </div>`
+//           });
+//         });
+//        });
   
-   }
-}
+//    }
+// }
 
 
-//seccion directores
-const directores = document.querySelectorAll(".menu_director");
+// //seccion directores
+// const directores = document.querySelectorAll(".menu_director");
 
-directores.forEach((director =>{
-   const directorI = director;
+// directores.forEach((director =>{
+//    const directorI = director;
   
-   directorI.addEventListener("click", () =>{
-      const idDirector = directorI.id;
-      const dataDirector = dataGhibli.filtrarDirectores(idDirector);
-      console.log(dataDirector);
-   })
-}))
+//    directorI.addEventListener("click", () =>{
+//       const idDirector = directorI.id;
+//       const dataDirector = dataGhibli.filtrarDirectores(idDirector);
+//       console.log(dataDirector);
+//    })
+// }))
 
 
 
