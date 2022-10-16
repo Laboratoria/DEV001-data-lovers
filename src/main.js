96640415d064b8f498109a3cd89bf641f891c743
OnXochitl <xhitl@hotmail.es>
2022-10-15 23:08:18 -0500

    Mostrar nombres de productores

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ const gFilms=data.films;
 
 
     //console.log(data["films"][k]["producer"]);
-    for (let i=0; i<filterData(gFilms,"producer").length;i++)
-    {
-        
-        console.log( filterData(gFilms,"producer")[i]);
-    }
+    const productor = document.querySelector(".productores"); //aquí accedo al div con el class = "productores"
+    const mostrarProductor = document.createDocumentFragment();
 
+    for (let i=0; i<filterData(gFilms,"producer").length;i++) {
+        const item = document.createElement("DIV"); //Esto crea un elemento de HTML (es en mayúscula porque esta sensible)
+        item.innerHTML = filterData(gFilms,"producer")[i]; //esto lo va guardando en un div
+        mostrarProductor.appendChild(item); //esto crea el div
+    }
+    
+    productor.appendChild(mostrarProductor); //y aquí se muestra
