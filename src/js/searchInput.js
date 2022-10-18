be00d0ddfa0dce847d677f6e39603cea23c00d03
gabyzam <ruth.gabriela.pz@gmail.com>
2022-10-17 23:05:47 -0500

    Realicé modificaciones en la función seachPokemon

--- src/js/searchInput.js
+++ src/js/searchInput.js
@@ -1,25 +1,5 @@
 const searchInputName = document.getElementById("input-search-name");
 
-/*const searchInput = {
-  searchPokemonByName: (arrayContainerCards, arrayNamePokemons) => {
-    searchInputName.addEventListener("input", () => {
-  
-        arrayNamePokemons?.forEach((name, index) => {
-          if (
-            !name.innerText
-              .toLowerCase()
-              .includes(searchInputName.value.toLowerCase())
-          ) {
-            arrayContainerCards[index].classList.add("hideCard");
-          } else {
-            arrayContainerCards[index].classList.remove("hideCard");
-          }
-        });
-
-    });
-  },
-}; */
-
 
 const searchInput = {
   searchPokemonByName: (arrayContainerCards, arrayNamePokemons) => {
