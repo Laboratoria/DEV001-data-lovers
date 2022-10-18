9dcea223589a78f5aa02b3b6713144e298df2dd2
gabyzam <ruth.gabriela.pz@gmail.com>
2022-10-17 22:56:55 -0500

    Mejoré la funcionalidad del buscador y valide solo letras

--- src/js/searchInput.js
+++ src/js/searchInput.js
@@ -1,19 +1,53 @@
 const searchInputName = document.getElementById("input-search-name");
 
+/*const searchInput = {
+  searchPokemonByName: (arrayContainerCards, arrayNamePokemons) => {
+    searchInputName.addEventListener("input", () => {
+  
+        arrayNamePokemons?.forEach((name, index) => {
+          if (
+            !name.innerText
+              .toLowerCase()
+              .includes(searchInputName.value.toLowerCase())
+          ) {
+            arrayContainerCards[index].classList.add("hideCard");
+          } else {
+            arrayContainerCards[index].classList.remove("hideCard");
+          }
+        });
+
+    });
+  },
+}; */
+
+
 const searchInput = {
   searchPokemonByName: (arrayContainerCards, arrayNamePokemons) => {
     searchInputName.addEventListener("input", () => {
-      arrayNamePokemons.forEach((name, index) => {
-        if (
-          !name.innerText
-            .toLowerCase()
-            .includes(searchInputName.value.toLowerCase())
-        ) {
-          arrayContainerCards[index].classList.add("hideCard");
-        } else {
-          arrayContainerCards[index].classList.remove("hideCard");
-        }
-      });
+
+      let inputValue = searchInputName.value.toLowerCase();
+      const onlyLetters = /^[/^[-_ a-zA-Z]+$/;
+      const nameValidate = onlyLetters.test(inputValue);
+
+      if (nameValidate === true || nameValidate === " "){
+        arrayNamePokemons?.forEach((name, index) => {
+          if (
+            !name.innerText
+              .toLowerCase()
+              .includes(inputValue)
+          ) {
+            arrayContainerCards[index].classList.add("hideCard");
+          } else {
+            arrayContainerCards[index].classList.remove("hideCard");
+          }
+        });
+
+        document.querySelector("#text-error").style.display = "none";
+
+      }else if (nameValidate === false){
+        document.querySelector("#text-error").style.display = "flex";
+      }
+
     });
   },
 };
