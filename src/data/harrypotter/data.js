1cffb0fe1fc8528d4d032b33d4de410709bde5cb
nicole11robayo <113997899+nicole11robayo@users.noreply.github.com>
2022-10-18 17:27:24 -0500

    portada completa

--- src/data/harrypotter/data.js
+++ src/data/harrypotter/data.js
@@ -16174,6 +16174,7 @@ export default {
       "title": "Harry Potter and the Sorcerer's Stone",
       "releaseDay": "June 26, 1997",
       "author": "J. K. Rowling",
+      "portada":"https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYLaPiedraFilosofal.jpg",
       "description": "On his birthday, Harry Potter discovers that he is the son of two well-known wizards, from whom he has inherited magical powers. He must attend a famous school of magic and sorcery, where he establishes a friendship with two young men who will become his companions on his adventure. During his first year at Hogwarts, he discovers that a malevolent and powerful wizard named Voldemort is in search of a philosopher's stone that prolongs the life of its owner.",
     },
     {
@@ -16181,6 +16182,7 @@ export default {
       "title": "Harry Potter and the chamber of secrets",
       "releaseDay": "July 2, 1998",
       "author": "J. K. Rowling",
+      "portada": "https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYLaCamaraSecreta.jpg",
       "description": "Harry Potter and the sophomores investigate a malevolent threat to their Hogwarts classmates, a menacing beast that hides within the castle.",
   
     },
@@ -16189,6 +16191,7 @@ export default {
       "title": "Harry Potter and the Prisoner of Azkaban",
       "releseDay": "July 8, 1999",
       "author": "J. K. Rowling",
+      "portada": "https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYElPrisioneroDeAzkaban.jpg",
       "description": "Harry's third year of studies at Hogwarts is threatened by Sirius Black's escape from Azkaban prison. Apparently, it is a dangerous wizard who was an accomplice of Lord Voldemort and who will try to take revenge on Harry Potter.",
     },
     {
@@ -16196,6 +16199,7 @@ export default {
       "title": "Harry Potter and the Goblet of Fire",
       "releaseDay": "July 8, 2000",
       "author": "J. K. Rowling",
+      "portada":"https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYElCalizDeFuego.jpg",
       "description": "Hogwarts prepares for the Triwizard Tournament, in which three schools of wizardry will compete. To everyone's surprise, Harry Potter is chosen to participate in the competition, in which he must fight dragons, enter the water and face his greatest fears.",
     },
     {
@@ -16203,6 +16207,7 @@ export default {
       "title": "Harry Potter and the Order of the Phoenix",
       "releaseDay": "June 21, 2003",
       "author": "J. K. Rowling",
+      "portada": "https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYLaOrdenDelFenix.jpg",
       "description": "In his fifth year at Hogwarts, Harry discovers that many members of the wizarding community do not know the truth about his encounter with Lord Voldemort. Cornelius Fudge, Minister of Magic, appoints Dolores Umbridge as Defense Against the Dark Arts teacher because he believes that Professor Dumbledore plans to take over her job. But his teachings are inadequate, so Harry prepares the students to defend the school against evil.",
     },
     {
@@ -16210,6 +16215,7 @@ export default {
       "title": "Harry Potter and the Half-Blood Prince",
       "releaseDay": "July 16, 2005",
       "author": "J. K. Rowling",
+      "portada":"https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYElMisterioPrincipe.jpg",
       "description": "Harry discovers a powerful book and, while trying to discover its origins, collaborates with Dumbledore in the search for a series of magical objects that will aid in the destruction of Lord Voldemort.",
     },
     {
@@ -16217,6 +16223,7 @@ export default {
       "title": "Harry Potter and the Deathly Hallows",
       "releaseDay": "July 21, 2007",
       "author": "J. K. Rowling",
+      "portada": "https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYLasReliquiasDeLaMuerte.jpg",
       "description": "Harry, Ron and Hermione go on a dangerous mission to locate and destroy the secret of Voldemort's immortality and destruction - the Horcruces. Alone, without the guidance of their teachers or the protection of Professor Dumbledore, the three friends must lean on each other more than ever. But there are Dark Forces in between that threaten to tear them apart. Harry Potter is getting closer and closer to the task for which he has been preparing since the first day he set foot in Hogwarts: the last battle with Voldemort.",
     },
     {
@@ -16224,6 +16231,7 @@ export default {
       "title": "Harry Potter and the Cursed Child",
       "releaseDay": "July 30, 2016",
       "author": "J. K. Rowling",
+      "portada": "https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYElLegadoMaldito.jpg",
       "description": "Harry's second son entered Hogwarts, but in Slytherin. His relationship with Harry is getting worse and he became close friends with Draco's son, Scorpius Malfoy who is said to be Lord Voldemort's son.",
     }
   ]
--- src/main.js
+++ src/main.js
@@ -18,42 +18,54 @@ let tablaProducto = document.getElementById('libros');
 let cuerpoTabla = document.createElement('tbody');
 let cuerpoTabla2 = document.createElement('tbody');
 
+/*
+let portadas=["https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYElMisterioPrincipe.jpg","https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYElLegadoMaldito.jpg","https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYLaCamaraSecreta.jpg", "https://raw.githubusercontent.com/nicole11robayo/DEV001-data-lovers/main/src/src_imagenes/HarryPotterYLaOrdenDelFenix.jpg"
+];
+*/
 
 p4.forEach(p => {
     //console.log(p.id);
-    let fila = document.createElement('tfood');
+    let fila = document.createElement('td');
     
             
     let td = document.createElement('tr');
-    td.innerText = p.id;
-    fila.appendChild(td);
-
-    td = document.createElement('tr');
     td.innerText = p.title;
     fila.appendChild(td);
 
-
     let img = document.createElement('img');
-    img.src = "https://m.media-amazon.com/images/I/51jYgLTHC8L._SX313_BO1,204,203,200_.jpg";
+    img.src = p.portada;
+    img.height= 400;
+    img.style.display="block";
+    img.style.margin="auto";
     fila.appendChild(img);
+
+    
+   
 
   
     
     cuerpoTabla.appendChild(fila);
+
 });
 tablaProducto.appendChild(cuerpoTabla);
+
+
 u4.forEach(p => {
     //console.log(p.id);
-    let fila2 = document.createElement('tfood');
+    let fila2 = document.createElement('td');
 
             
     let td2 = document.createElement('tr');
-    td2.innerText = p.id;
-    fila2.appendChild(td2);
-    
-    td2 = document.createElement('tr');
     td2.innerText = p.title;
     fila2.appendChild(td2);
+
+    let img = document.createElement('img');
+    img.src = p.portada;
+    img.height= 400;
+    img.style.display="block";
+    img.style.margin="auto";
+    fila2.appendChild(img);
+
 
     cuerpoTabla2.appendChild(fila2);
 });
