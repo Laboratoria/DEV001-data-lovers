1e314ba809c5057f829ade26559f604bad411f49
Danica <danicagarcia1613@gmail.com>
2022-10-20 12:05:34 -0500

    traer data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {getBooks} from './data.js';
+import harrypotter from "./data/harrypotter/data.js"
 
-console.log(example, data);
+
+const  addElement =() =>{
+  let newDiv = document.getElementById("listas");
+  getBooks().forEach(book => {
+    
+    let newContent = document.createElement("h1");
+  newContent.innerHTML=`<span>${book.title}</span>`
+  newDiv.appendChild(newContent); 
+});
+}
+
+const buttonPelis= document.getElementById("peliculas");
+
+buttonPelis.addEventListener("click",()=>{
+    addElement();
+    document.getElementById("libros").style.display = 'none';
+    document.getElementById("peliculas").style.display = 'none';
+    document.getElementById("precuelas").style.display = 'none';
+    document.getElementById("deporte").style.display = 'none';
+    document.getElementById("foto").style.display = 'none';
+    document.body.style.backgroundImage='none';
+})
