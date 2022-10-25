533ae4a8577ec2cda2390021fa27ef88d1622627
karlaCRM <abraxaskrm@gmail.com>
2022-10-24 19:10:12 -0500

    cambios

--- src/showCards.js
+++ src/showCards.js
@@ -95,7 +95,7 @@ export let showPokemonFeature = (objectData) => {
   <section class="evolution-pokemon">
     <h2>Evolution:</h2>
     <div class="evolution-pokemon-1" id="evolution-1"> 
-    
+      ${evolutions(objectData)}
     </div>
   </section>
   </div>
@@ -103,6 +103,10 @@ export let showPokemonFeature = (objectData) => {
 
 containerCharacteristics.insertAdjacentHTML("beforeend", pokemonFeatures);
 
+}
+
+let evolutions= ()=>{
+  
 }
 
 /*<p>${objectData.evolution["next-evolution"][0].num}</p>
