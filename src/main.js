04a66178cdede01381b9c2aec8d1616c1e057e3e
laurybeth <laurybeth@msn.com>
2022-10-24 21:48:38 -0500

    foreach en producers de main.js

--- src/main.js
+++ src/main.js
@@ -25,10 +25,10 @@ const gFilms=data.films;
     const producer = document.querySelector(".producers"); //aquí accedo al div con el class = "productores"
     const showProducer = document.createDocumentFragment();
 
-    for (let i=0; i<filterData(gFilms,"producer").length;i++) {
+    filterData(gFilms,"producer").forEach(function (value) {
         const item = document.createElement("DIV"); //Esto crea un elemento de HTML (es en mayúscula porque es sensible)
-        item.innerHTML = filterData(gFilms,"producer")[i]; //esto lo va guardando en un div
+        item.innerHTML = value; //esto lo va guardando en un div
         showProducer.appendChild(item); //esto crea el div
-    }
+      });
     
     producer.appendChild(showProducer); //y aquí se muestra
