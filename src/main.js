85155846a658b24923ef8e3a7ddc3aa47a32b75b
Silvana Gonzalez <algomio2000@gmail.com>
2022-10-25 11:39:53 -0500

    Probando estilos

--- src/main.js
+++ src/main.js
@@ -1,8 +1,2 @@
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
