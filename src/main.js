540a08c95b95c9deb505c522da85e2ee7a7ed077
Silvana Gonzalez <algomio2000@gmail.com>
2022-10-27 18:00:21 -0500

    subiendo cambios

--- src/main.js
+++ src/main.js
@@ -5,21 +5,20 @@
 import data from './data/pokemon/pokemon.js';
 
 //AQUÍ YA LOGRÉ QUE SALGA EL ÚLTIMO POKEMON, IMAGEN, NOMBRE Y NUM usando: import data from './data/pokemon/pokemon.js';
-const cardInfo = document.getElementById("container")
+const cardInfo = document.getElementById("container");
 const elementPokemon = document.createElement("div");
 const infoPokemon = data.pokemon;
 
 infoPokemon.forEach((card) => {
   const pokemonHTML = `
   <div class="cont.cards" id="container">
-  <div class="card" id="card">
-  <img class="img" src=${card.img} </img>
-  </div>
-  <div> 
-  <p><class= "namepokemon">${card.name}</p>
-  <p><class= "numpokemon">${card.num}</p>
-  </div>
-  
+     <div class="card" id="card">
+      <img class="img" src=${card.img} </img>
+   </div>
+      <div> 
+        <p><class= "namepokemon">${card.name}</p>
+        <p><class= "numpokemon">${card.num}</p>
+      </div>
   </div>
   `;
   elementPokemon.innerHTML += pokemonHTML
