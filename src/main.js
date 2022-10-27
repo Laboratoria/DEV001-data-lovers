742c6e2e5783094c5b5f8b1f3efb848a279c38bf
MelissaRV <melissa.rvillegas206@gmail.com>
2022-10-27 12:57:19 -0500

    segunda página de html

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import objetoghibli from './data/ghibli/ghibli.js';
 import objetoghibli from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, objetoghibli);
+
+console.log(objetoghibli);
+
