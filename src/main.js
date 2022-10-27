43f987fda5e3c690016bc9125b90c341dbd7f89b
Karla GaSa <karlagsanabria@gmail.com>
2022-10-27 14:04:21 -0500

    En este commit se sube el CSS para ver en cuadrilla y el JS con los últimos cambios.

--- src/main.js
+++ src/main.js
@@ -1,27 +1,23 @@
-// import {data} from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-//import pokemon from './data.js';
-//import pokemon from './data/pokemon/pokemon.js';
+
 import data from './data/pokemon/pokemon.js';
 
 //AQUÍ YA LOGRÉ QUE SALGA EL ÚLTIMO POKEMON, IMAGEN, NOMBRE Y NUM usando: import data from './data/pokemon/pokemon.js';
-const cardInfo = document.getElementById("container")
+const cardInfo = document.getElementById("cont-card");
 const elementPokemon = document.createElement("div");
+elementPokemon.classList.add("solucion")
 const infoPokemon = data.pokemon;
 
 infoPokemon.forEach((card) => {
   const pokemonHTML = `
-  <div class="cont.cards" id="container">
-  <div class="card" id="card">
-  <img class="img" src=${card.img} </img>
-  </div>
-  <div> 
-  <p><class= "namepokemon">${card.name}</p>
+  <div class="cont.card" id=${card.num}>
+  <div class="card" id="card"> <img class="img" src=${card.img} </img><div> 
+  <p><class= "namepokemon">${card.name} </p>
   <p><class= "numpokemon">${card.num}</p>
   </div>
-  
+  </div>
   </div>
   `;
+
   elementPokemon.innerHTML += pokemonHTML
   cardInfo.appendChild(elementPokemon)
   //console.log(pokemonParaHTML)
