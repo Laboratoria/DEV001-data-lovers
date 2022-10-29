a3a3041a67ac08cfef3a088dc8433f36d654e9c6
OnXochitl <xhitl@hotmail.es>
2022-10-28 23:49:29 -0500

    Se muestran las peliculas y se crea nuevo apartado Movies en el menu

--- src/data.js
+++ src/data.js
@@ -2,12 +2,33 @@ export const filterData = (data, condition) => {
   const result = [];
 
   data.filter((data) => {
+
     if (!result.includes(data[condition])) { result.push(data[condition]); }
 
     return result;
 
   });
-
   return result;
 };
 
+/**
+ * Ordena las peliculas por titulo en orden ascendente(a-z) por defecto
+ * @param {array} movies
+ * @param {string} order(default asc)
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#sorting_array_of_objects}
+ * @returns {array}
+ */
+export function orderByTitle(movies) {
+  return [...movies].sort((movieLeft, movieRight) => {
+      // el titulo de la izquierda va primero
+      if (movieLeft.title < movieRight.title) {
+        return -1;
+      }
+      // el titulo de la derecha va primero
+      if (movieLeft.title > movieRight.title) {
+        return 1;
+      }
+      // los titulos son iguales
+      return 0;
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-import { filterData} from './data.js';
+import { filterData } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
-const gFilms=data.films;
+const gFilms = data.films;
 
 //console.log(data["films"][0]["producer"]);
 
-    const iconHamburger = document.getElementById("hamburgerIcon");
+const iconHamburger = document.getElementById("hamburgerIcon");
 
-    iconHamburger.addEventListener('click', showMenuLeftResponsive);
-    
-    function showMenuLeftResponsive () {
-        
+iconHamburger.addEventListener('click', showMenuLeftResponsive);
+
+function showMenuLeftResponsive() {
+
     var x = document.getElementById("menuLeft");
     if (x.className === "menuLeft") {
         x.className += " responsive";
@@ -19,16 +19,19 @@ const gFilms=data.films;
         x.className = "menuLeft";
     }
 }
-      
 
-    //console.log(data["films"][k]["producer"]);
-    const producer = document.querySelector(".producers"); //aquí accedo al div con el class = "productores"
-    const showProducer = document.createDocumentFragment();
 
-    filterData(gFilms,"producer").forEach(function (value) {
-        const item = document.createElement("DIV"); //Esto crea un elemento de HTML (es en mayúscula porque es sensible)
-        item.innerHTML = value; //esto lo va guardando en un div
-        showProducer.appendChild(item); //esto crea el div
-      });
-    
-    producer.appendChild(showProducer); //y aquí se muestra
+//console.log(data["films"][k]["producer"]);
+const producer = document.getElementById("productores"); //aquí accedo al div con el class = "productores"
+const showProducer = document.createDocumentFragment();
+
+filterData(gFilms, "producer").forEach(function (value) {
+    const item = document.createElement("DIV"); //Esto crea un elemento de HTML (es en mayúscula porque es sensible)
+    item.innerHTML = value; //esto lo va guardando en un div
+    showProducer.appendChild(item); //esto crea el div
+});
+
+producer.appendChild(showProducer); //y aquí se muestra
+
+
+
