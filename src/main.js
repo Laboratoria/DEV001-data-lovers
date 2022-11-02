da7ec8f21ffdd88eca365b967306ea148d6089bc
Karla GaSa <karlagsanabria@gmail.com>
2022-11-02 10:58:52 -0600

    Este commit contiene las cartas en grilla, con fondo estrelaldo, ya es responsiva la página y tiene formato general de CSS.

--- src/main.js
+++ src/main.js
@@ -7,20 +7,22 @@ const elementPokemon = document.createElement("div");
 elementPokemon.classList.add("solucion")
 const infoPokemon = data.pokemon;
 
-infoPokemon.forEach((card) => {
-  const pokemonHTML = `
-  <div class="cont.card" id=${card.num}>
-  <div class="card" id="card"><img class="img" src=${card.img} </img><div> 
-  <p><class= "namepokemon">${card.name} </p>
-  <p><class= "numpokemon">${card.num}</p>
+//TODO: creare funcion que reciba un arreglo de objetos(pokemones) y retorna un arreglo de cards (etiquetas HTML)
 
-  </div>
-  </div>
-  </div>
-  `;
+  infoPokemon.forEach((card) => {
+    const pokemonHTML = `
+    <div class="cont.card" id=${card.num}>
+    <img class="detalle" id="circulo"></img>
+    <div class="card" id="card"><img class="img" src=${card.img} </img>
+    <p><class= "namepokemon">${card.name} </p>
+    <p><class= "numpokemon">${card.num}</p>
+    </div>
+    </div>
+    `;
 
-  elementPokemon.innerHTML += pokemonHTML
-  cardInfo.appendChild(elementPokemon)
-  //console.log(pokemonParaHTML)
-}
-);
+    elementPokemon.innerHTML += pokemonHTML
+    cardInfo.appendChild(elementPokemon)
+    //console.log(pokemonParaHTML)
+  }
+  );
+
