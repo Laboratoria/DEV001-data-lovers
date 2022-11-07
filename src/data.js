b7825a38a38820adfc9779e6e648f8c69a4c1818
unknown <catalina.ordenes@mail.udp.cl>
2022-11-07 13:44:29 -0300

    Primera historia de usuario

--- src/data.js
+++ src/data.js
@@ -1,9 +1,24 @@
 // estas funciones son de ejemplo
+import data from "./data/pokemon/pokemon.js";
+
+
+
+
+//     return name.isla === 'kanto'
+// });
+// console.log(pokeKanto);
+
+
+
+
+
+
+
 
 export const example = () => {
-  return 'example';
+    return 'example';
 };
 
 export const anotherExample = () => {
-  return 'OMG';
+    return 'OMG';
 };
--- src/main.js
+++ src/main.js
@@ -2,5 +2,33 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+let pokeKanto = []
+data.pokemon.forEach(function(pokemon) {
+    if (pokemon.generation.name === "kanto") {
+        pokeKanto.push(pokemon);
+    }
+})
+console.log(pokeKanto)
+const tarjeta = document.getElementById("contenedor");
 
-console.log(example, data);
+const mostrartodo = (mostrar) => {
+    mostrar.forEach((hola) => {
+        const div = document.createElement("div"); //creamos un div
+        div.className = "tarjeta"; //Le agregamos una clase
+        const imagen = document.createElement("img"); //creo una variable para las fotos de la data
+        const nombre = document.createElement("p"); //el nombre del pokemon de la data
+        const numero = document.createElement("p"); //el número del pokemon extraído de la data
+        numero.className = "numero"
+        imagen.className = "imagen" //otorgamos una clase
+        nombre.className = "nombre"
+        imagen.src = `${hola.img}`
+        nombre.innerHTML = `${hola.name}`
+        numero.innerHTML = `${hola.num}`
+        div.appendChild(numero)
+        div.appendChild(imagen)
+        div.appendChild(nombre)
+        tarjeta.appendChild(div)
+
+    })
+}
+mostrartodo(pokeKanto)
