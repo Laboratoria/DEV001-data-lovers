69a3286a524e53abe354141c3163755fe5f16d15
unknown <catalina.ordenes@mail.udp.cl>
2022-11-15 15:41:08 -0300

    Comienzo de la etapa de filtrado del array nuevo con los pokemones de Kanto

--- src/data.js
+++ src/data.js
@@ -1,12 +1,28 @@
-// estas funciones son de ejemplo
-import data from "./data/pokemon/pokemon.js";
+export function filtroPokeKanto(pokemones) {
+    let pokeKanto = []
+    pokemones.forEach(function(pokemon) {
+        if (pokemon.generation.name === "kanto") {
+            pokeKanto.push(pokemon);
+        }
+    })
+    return pokeKanto;
+
+}
 
 
 
 
-//     return name.isla === 'kanto'
-// });
-// console.log(pokeKanto);
+
+
+
+//realizar test a esta función
+//Realizar una función que permita filtrar dependiendo lo que el usuario eligió
+//A pokeKanto se le hace el filter
+
+//Se tendría que exportar la const que utilizaremos en el test?
+
+//hacer un filter con un forEach para que aplique a cada elemento de la data
+//Hacer que se borre la pantalla y que aparezca el resultado de filter
 
 
 
@@ -14,11 +30,10 @@ import data from "./data/pokemon/pokemon.js";
 
 
 
+// export const example = () => {
+//     return 'example';
+// };
 
-export const example = () => {
-    return 'example';
-};
-
-export const anotherExample = () => {
-    return 'OMG';
-};
+// export const anotherExample = () => {
+//     return 'OMG';
+// };
